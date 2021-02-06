fa67c970e7fb24325f120f15a71bd812b07bf305
Alexander Cherkas <folickthebest@gmail.com>
2021-02-06 09:50:33 +0200

    fix container / add slider

--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,12 @@ import './vendor/_slick.js';
 
 $(function () {
 
+  $('.hero__slider').slick({
+    slidesToShow: 1,
+    infinite: true,
+    speed: 300,
+    dots: true,
+    arrows: false,
+  });
+
 });
