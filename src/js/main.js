ae03c5cce120a919fa76936a79b69cf65d22d188
Alexander Cherkas <folickthebest@gmail.com>
2021-02-06 10:22:38 +0200

    create product slider

--- src/js/main.js
+++ src/js/main.js
@@ -10,9 +10,15 @@ $(function () {
   $('.hero__slider').slick({
     slidesToShow: 1,
     infinite: true,
-    speed: 300,
     dots: true,
     arrows: false,
+  });
+
+  $('.product__slider').slick({
+    slidesToShow: 5,
+    infinite: true,
+    prevArrow: `<button class="btn-reset slick-prev slick-arrow"><svg><use xlink:href="./images/sprite.svg#slide-prev"></use></svg></button>`,
+    nextArrow: `<button class="btn-reset slick-next slick-arrow"><svg><use xlink:href="./images/sprite.svg#slide-next"></use></svg></button>`,
   });
 
 });
